47e248ae8fc29cb0ab6e15f679f5e1401fd3abd4
aditya-0685 <165156827+aditya-0685@users.noreply.github.com>
2024-05-24 15:40:25 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+let tablinks = document.getElementsByClassName("tab-links");
+let tabcontents = document.getElementsByClassName("tab-contents");
+function opentab(tabname){
+    for(tablink of tablinks){
+        tablink.classList.remove("active-links");
+    }
+    for(tabcontent of tabcontents){
+        tabcontent.classList.remove("active-tab");
+    }
+    event.currentTarget.classList.add("active-link");
+    document.getElementById(tabname).classList.add("active-tab");
+}
+var sidemenu = document.getElementById("sidemenu");
+function openmenu(){
+     sidemenu.style.right = "0"; // for menu open
+}
+function closemenu(){
+     sidemenu.style.right = "-200px"; // for menu close
+}
+
